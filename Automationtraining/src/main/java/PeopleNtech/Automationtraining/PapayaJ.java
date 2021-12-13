998f54fd5cfcd959f1d670d1dbef7bce94a97667
ahlam202 <ahlam.larguet@gmail.com>
2021-12-13 16:53:31 -0600

    first

--- Automationtraining/src/main/java/PeopleNtech/Automationtraining/PapayaJ.java
+++ Automationtraining/src/main/java/PeopleNtech/Automationtraining/PapayaJ.java
@@ -3,6 +3,7 @@ package PeopleNtech.Automationtraining;
 
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -19,7 +20,8 @@ public class PapayaJ {
 		driver=new ChromeDriver();
 		driver.navigate().to("https://www.papajohns.com/");
 		driver.manage().window().maximize();
-		Thread.sleep(6000);
+		driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+		
 		WebElement StartOrder=driver.findElement(By.xpath("//*[@href=\" /order/stores-near-me\"]"));
 		StartOrder.click();
 		Thread.sleep(6000);
@@ -30,11 +32,11 @@ public class PapayaJ {
 		Select addtype=new Select(bus);
 		addtype.selectByValue("BUSINESS");
 	    WebElement street= driver.findElement(By.id("autocomplete"));
-	    street.sendKeys("austin");
+	    street.sendKeys("123 Red River ");
 	    Thread.sleep(6000);
 		List<WebElement> allstreet= driver.findElements(By.id("autocomplete__listbox"));
 		for (WebElement Element : allstreet) {
-			if (Element.getText().equalsIgnoreCase("Austin, TX, USA")) {
+			if (Element.getText().equalsIgnoreCase("123 Red River Street, Austin, TX, USA")) {
 				Element.click();
 				break;
 			}
@@ -50,6 +52,19 @@ public class PapayaJ {
 		zip.sendKeys("78701");
 		WebElement butt=driver.findElement(By.xpath("//*[@class=\"button button-large\"]"));
 		butt.click();
+		WebElement addOrder= driver.findElement(By.xpath("(//*[@aria-label=\"ADD TO ORDER Cheese Pizza\"])[2]"));
+		addOrder.click();
+		WebElement drink= driver.findElement(By.id("drinks"));
+		Select s=new Select(drink);
+		s.selectByVisibleText("Diet Pepsi");
+		WebElement add=driver.findElement(By.xpath("//*[@aria-label=\"2 Liter Add to Order\"]"));
+		add.click();
+		Thread.sleep(5000);
+		WebElement shopCart= driver.findElement(By.xpath("//*[@id=\"cart\"]"));
+		shopCart.click();
+		WebElement checkout= driver.findElement(By.id("checkout-button"));
+	     checkout.click();
+	     
 		
 		
 		
